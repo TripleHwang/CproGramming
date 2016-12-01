c1dbbcd9517ae958058404e19713c153a37d83e1
TripleHwang <hwanghhh82@gmail.com>
2016-12-01 23:21:39 +0900

    new strcatptr.c

--- pointer/Practice11/strcatptr.c
+++ pointer/Practice11/strcatptr.c
@@ -3,15 +3,16 @@
 /* strcat: concatenate t to end of s;
            s must be big enough to hold the two */
 void mystrcat(char *s, char *t) {
-	while (*s != '\0')
-		s++;
-	while (*t != '\0') {
-		*s++ = *t++;
-	}
+  while (*s != '\0') /* find end of s */
+    s++;
+  while ((*s = *t) != '\0'){ /* copy t */
+    s++;
+    t++;
+  }
 }
 
 int main() {
-    char x[100] = "Hanyang";
-    mystrcat(x, "ERICA");
-    printf("%s\n", x);
+  char x[100] = "Hanyang";
+  mystrcat(x, "ERICA");
+  printf("%s\n", x);
 }
