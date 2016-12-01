703b6a6f16466608948190218314048acf7f8114
TripleHwang <hwanghhh82@gmail.com>
2016-12-01 23:21:16 +0900

    new strcat

--- pointer/Practice11/strcat.c
+++ pointer/Practice11/strcat.c
@@ -3,17 +3,17 @@
 /* strcat: concatenate t to end of s;
            s must be big enough to hold the two */
 void mystrcat(char s[], char t[]) {
-    int i, j;
+  int i, j;
 
-    i = j = 0;
-    while (s[i] != '\0') /* find end of s */
-        i++;
-    while ((s[i++] = t[j++]) != '\0') /* copy t */
-        ;
+  i = j = 0;
+  while (s[i] != '\0') /* find end of s */
+    i++;
+  while ((s[i++] = t[j++]) != '\0') /* copy t */
+    ;
 }
 
 int main() {
-	char x[100] = "Hanyang";
-	mystrcat(x, "ERICA");
-	printf("%s\n", x);
+  char x[100] = "Hanyang";
+  mystrcat(x, "ERICA");
+  printf("%s\n", x);
 }
